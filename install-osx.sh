93172d730d955aa9de39ed9bc52de5292c4a6485
mir-am <mir-am@hotmail.com>
2019-03-09 18:10:27 +0330

    Test the LightTwinSVM on OSX.

---
+++ install-osx.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# LightTwinSVM Program - Simple and Fast
+# Version: 0.1.0-alpha - 2018-05-24
+# Developer: Mir, A. (mir-am@hotmail.com)
+# License: GNU General Public License v3.0
+
+# In this shell script, required dependencis will be installed.
+# for building and testing LightTwinSVM on Travis CI
+
+python --version
+echo "OS: $OSTYPE"
